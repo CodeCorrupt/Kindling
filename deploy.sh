5e0fbfee5ea0ffd04fc5fbe24bcd5cca61014791
CodeCorrupt <tylerhoyt93@gmail.com>
2024-10-04 23:09:34 -0400

    Switch plex to bridge instead of host networking

--- deploy.sh
+++ deploy.sh
@@ -2,3 +2,4 @@
 # This isn't much of a script to share. Really just making my life easy....
 
 docker -H ssh://media-server compose up -d --force-recreate plex
+docker -H ssh://media-server compose logs -f --tail 100

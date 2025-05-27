6ac0a1a90b5080037fabc6df924c0ab8ec4a2fba
CodeCorrupt <tylerhoyt93@gmail.com>
2025-05-26 23:18:46 -0400

    Update to deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # This isn't much of a script to share. Really just making my life easy....
 
-docker -H ssh://192.168.1.61 compose up -d --force-recreate --pull=always
-#docker -H ssh://media-server compose logs -f --tail 100
+docker -H ssh://media-server.lan compose up -d --force-recreate --pull=always
+docker -H ssh://media-server.lan compose logs -f --tail 100
